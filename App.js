ad0dd10cf32e3184a69c042a9a073249e12b248f
Joey Yu <itsjoeoui@gmail.com>
2021-03-30 19:07:54 -0400

    Init Firebase

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as firebase from 'firebase';
+
+// Initialize Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyAwWy-UfGf_IUstwX1x8V64lLW1AgfLai8",
+  authDomain: "treehugger-e8c37.firebaseapp.com",
+  databaseURL: "https://treehugger-e8c37-default-rtdb.firebaseio.com",
+  projectId: "treehugger-e8c37",
+  storageBucket: "treehugger-e8c37.appspot.com",
+  messagingSenderId: "467428435573",
+  appId: "1:467428435573:web:42c9877057d1b7effafa3c"
+};
+
+if (firebase.apps.length === 0) {
+  firebase.initializeApp({});
+}
 
 export default function App() {
   return (
