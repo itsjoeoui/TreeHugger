27370d19c8f0eadacda620abd474a8fb588188c6
Joey Yu <itsjoeoui@gmail.com>
2021-04-02 00:06:28 -0400

    Make the tree interactive using Model
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,9 +1,10 @@
-import React from 'react';
-import { Dimensions, StyleSheet, View, Image} from 'react-native';
+import React, { useState } from 'react';
+import { Dimensions, StyleSheet, View, Image, Modal, Text, Pressable } from 'react-native';
 import MapView, { Marker } from 'react-native-maps';
 import tree from '../assets/tree.png';
 
 const HomeScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View
       style={{ flex: 1, justifyContent: 'space-between', alignItems: 'center' }}
@@ -15,15 +16,38 @@ const HomeScreen = () => {
           longitude: -73.587916,
           latitudeDelta: 0.0043,
           longitudeDelta: 0.0034,
-        }}>
+        }}
+      >
         <Marker
           coordinate={{
             latitude: 45.489724,
             longitude: -73.587916,
-          }}>
+          }}
+          onPress={() => setModalVisible(!modalVisible)}
+        >
           <Image style={styles.tree} source={tree} />
         </Marker>
       </MapView>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          setModalVisible(!modalVisible);
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Hello World!</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}
+            >
+              <Text style={styles.textStyle}>Hide Modal</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
     </View>
   );
 }
@@ -35,9 +59,39 @@ const styles = StyleSheet.create({
     height: Dimensions.get('window').height,
   },
   tree: {
-      width: 40,
-      height: 40,
+    width: 50,
+    height: 50,
   },
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: "white",
+    borderRadius: 20,
+    padding: 35,
+    alignItems: "center",
+    elevation: 5
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2
+  },
+  buttonClose: {
+    backgroundColor: "#2196F3",
+  },
+  textStyle: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: "center"
+  }
 });
 
 export default HomeScreen;
