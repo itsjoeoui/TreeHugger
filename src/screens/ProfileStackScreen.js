be4d01209a1393f4070141680c14006832966d18
Shawn Maor <lital.maor@dawsoncollege.qc.ca>
2021-04-05 21:05:24 -0400

    UI update for the profile page
    * Sign in buttons are removed for now.
    
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- src/screens/ProfileStackScreen.js
+++ src/screens/ProfileStackScreen.js
@@ -1,7 +1,17 @@
 import React from "react";
-import { StyleSheet, View, TouchableOpacity } from "react-native";
-import { SocialIcon } from "react-native-elements";
+import {
+  StyleSheet,
+  View,
+  Image,
+  TouchableOpacity,
+  Text,
+  ScrollView,
+  Pressable,
+} from "react-native";
+// import { SocialIcon } from "react-native-elements";
 import { createStackNavigator } from "@react-navigation/stack";
+import newMessage from "../assets/newmessage.png";
+import one from "../assets/1.png";
 
 const ProfileStack = createStackNavigator();
 
@@ -13,33 +23,169 @@ const ProfileStackScreen = () => {
   );
 };
 
-const Profile = () => {
-  return (
-    <View style={styles.login}>
-      <TouchableOpacity>
-        <SocialIcon title="Sign in with Google" light button type="google" />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <SocialIcon title="Sign in with Twitter" light button type="twitter" />
-      </TouchableOpacity>
-      <TouchableOpacity>
-        <SocialIcon
-          title="Sign in with Facebook"
-          light
-          button
-          type="facebook"
-        />
-      </TouchableOpacity>
+const Profile = () => (
+  <ScrollView style={styles.scrollView}>
+    <UserInfo />
+    <View
+      style={{
+        flexDirection: "row",
+        marginTop: 30,
+        justifyContent: "space-between",
+      }}
+    >
+      <Text style={[styles.textCenter, { fontSize: 20 }]}>New Updates</Text>
+      <Pressable>
+        <Text style={{ fontSize: 16, textAlignVertical: "bottom" }}>
+          View All
+        </Text>
+      </Pressable>
     </View>
-  );
-};
+    <View style={styles.line} />
+    <Updates />
+    <Updates />
+    <View
+      style={{
+        flexDirection: "row",
+        marginTop: 30,
+        justifyContent: "space-between",
+      }}
+    >
+      <Text style={[styles.textCenter, { fontSize: 20 }]}>Achievements</Text>
+      <Pressable>
+        <Text style={{ fontSize: 16, textAlignVertical: "bottom" }}>
+          View All
+        </Text>
+      </Pressable>
+    </View>
+    <View style={styles.line} />
+    <Achievements />
+  </ScrollView>
+);
+
+const UserInfo = () => (
+  <View style={[styles.container, { justifyContent: "flex-start" }]}>
+    <View style={{ flexDirection: "row" }}>
+      <Image
+        style={styles.image}
+        source={{
+          uri:
+            "https://www.ajactraining.org/wp-content/uploads/2019/09/image-placeholder.jpg",
+        }}
+      />
+      <View
+        style={[
+          styles.container,
+          { flexDirection: "column", alignItems: "center" },
+        ]}
+      >
+        <Text
+          adjustsFontSizeToFit
+          numberOfLines={1}
+          style={[styles.textCenter, { fontSize: 26 }]}
+        >
+          John Johnson
+        </Text>
+        <Text style={[styles.textCenter, { fontSize: 16 }]}>Deeds: 23</Text>
+      </View>
+    </View>
+  </View>
+);
+
+const Updates = () => (
+  <TouchableOpacity>
+    <View
+      style={[
+        styles.container,
+        {
+          borderWidth: 1,
+          marginTop: 10,
+          backgroundColor: "white",
+          justifyContent: "flex-start",
+        },
+      ]}
+    >
+      <View style={{ flexDirection: "row" }}>
+        <Image
+          style={{ width: 50, height: 50, marginHorizontal: 10 }}
+          source={newMessage}
+        />
+        <View
+          style={[
+            styles.container,
+            { flexDirection: "column", alignItems: "center" },
+          ]}
+        >
+          <Text style={[styles.textCenter, { fontSize: 16 }]}>
+            New reply to your message on Tree!
+          </Text>
+        </View>
+      </View>
+    </View>
+  </TouchableOpacity>
+);
+
+const Achievements = () => (
+  <View
+    style={[
+      styles.container,
+      {
+        borderWidth: 1,
+        marginTop: 10,
+        backgroundColor: "white",
+        justifyContent: "flex-start",
+      },
+    ]}
+  >
+    <View style={{ flexDirection: "row", justifyContent: "flex-start" }}>
+      <Image
+        style={{
+          width: 45,
+          height: 45,
+          marginHorizontal: 10,
+          marginVertical: 5,
+        }}
+        source={one}
+      />
+      <View
+        style={[
+          styles.container,
+          { flexDirection: "column", alignItems: "center" },
+        ]}
+      >
+        <Text style={[styles.textCenter, { fontSize: 16 }]}>
+          Leave your first deed.
+        </Text>
+      </View>
+    </View>
+  </View>
+);
 
 const styles = StyleSheet.create({
-  login: {
-    width: "60%",
-    height: "100%",
+  scrollView: {
+    flex: 1,
+    margin: 15,
+  },
+  image: {
+    width: 80,
+    height: 80,
+    borderRadius: 85,
+    marginRight: 15,
+  },
+  textCenter: {
+    textAlign: "left",
+    textAlignVertical: "center",
+    alignSelf: "stretch",
+  },
+  container: {
+    flexDirection: "row",
+    flexWrap: "wrap",
     justifyContent: "center",
-    marginLeft: "20%",
+  },
+  line: {
+    padding: 0,
+    width: "100%",
+    alignSelf: "center",
+    borderWidth: 1,
   },
 });
 
