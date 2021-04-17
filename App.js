eef0c4c34e86c8f63df1b19fd948b6f1b0ac8184
Joey Yu <itsjoeoui@gmail.com>
2021-04-16 21:53:57 -0400

    Add back nav bar icons
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- App.js
+++ App.js
@@ -3,6 +3,7 @@ import * as React from "react";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons, MaterialCommunityIcons } from "@expo/vector-icons";
 import ChatScreen from "./src/screens/Chat";
 import HomeScreen from "./src/screens/Home";
 import ProfileScreen from "./src/screens/Profile";
@@ -13,7 +14,40 @@ const MainTab = createBottomTabNavigator();
 
 function HomeStack() {
   return (
-    <MainTab.Navigator>
+    <MainTab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused, color, size }) => {
+          let iconName;
+
+          if (route.name === "Home") {
+            iconName = focused ? "tree" : "tree-outline";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size * 1.2}
+                color={color}
+              />
+            );
+          } else if (route.name === "Profile") {
+            iconName = focused ? "face" : "face-outline";
+            return (
+              <MaterialCommunityIcons
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            );
+          } else if (route.name === "Settings") {
+            iconName = focused ? "settings" : "settings-outline";
+            return <Ionicons name={iconName} size={size} color={color} />;
+          }
+        },
+      })}
+      tabBarOptions={{
+        activeTintColor: "green",
+        inactiveTintColor: "gray",
+      }}
+    >
       <MainTab.Screen name="Home" component={HomeScreen} />
       <MainTab.Screen name="Profile" component={ProfileScreen} />
       <MainTab.Screen name="Settings" component={SettingsScreen} />
