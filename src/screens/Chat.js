da353b307d10f8d2f437fbab0ca08569ca804575
Joey Yu <itsjoeoui@gmail.com>
2021-04-18 09:55:57 -0400

    Implement Firebase for GitftedChat
    * Reference: https://snack.expo.io/@bacon/firebase-basic-chat
    * Convert Class Component to Function Component
    
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- src/screens/Chat.js
+++ src/screens/Chat.js
@@ -1,42 +1,37 @@
-import React, { useState, useCallback, useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { GiftedChat } from "react-native-gifted-chat";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const ChatScreen = () => {
-  const [messages, setMessages] = useState([]);
+import Fire from "./Fire";
+
+function ChatScreen() {
   const insets = useSafeAreaInsets();
+  const [messages, setMessages] = useState([]);
+  const user = {
+    _id: Fire.shared.uid,
+    name: "Joey",
+    avatar: "https://avatars0.githubusercontent.com/u/25695219?v=4",
+  };
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: "Hello World!",
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: "React Native",
-          avatar: "https://avatars0.githubusercontent.com/u/25695219?v=4",
-        },
-      },
-    ]);
-  }, []);
-
-  const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages)
+    Fire.shared.on((messages) =>
+      setMessages((previousMessages) =>
+        GiftedChat.append(previousMessages, messages)
+      )
     );
+    return () => {
+      Fire.shared.off();
+    };
   }, []);
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => onSend(messages)}
+      onSend={Fire.shared.send}
+      user={user}
       bottomOffset={insets.bottom}
-      user={{
-        _id: 1,
-      }}
     />
   );
-};
+}
 
 export default ChatScreen;
---
+++ src/screens/Fire.js
@@ -0,0 +1,85 @@
+import firebase from "firebase";
+
+class Fire {
+  constructor() {
+    this.init();
+    this.observeAuth();
+  }
+
+  init = () =>
+    firebase.initializeApp({
+      apiKey: "AIzaSyAwWy-UfGf_IUstwX1x8V64lLW1AgfLai8",
+      authDomain: "treehugger-e8c37.firebaseapp.com",
+      databaseURL: "https://treehugger-e8c37-default-rtdb.firebaseio.com",
+      projectId: "treehugger-e8c37",
+      storageBucket: "treehugger-e8c37.appspot.com",
+      messagingSenderId: "467428435573",
+      appId: "1:467428435573:web:42c9877057d1b7effafa3c",
+    });
+
+  observeAuth = () =>
+    firebase.auth().onAuthStateChanged(this.onAuthStateChanged);
+
+  onAuthStateChanged = (user) => {
+    if (!user) {
+      try {
+        firebase.auth().signInAnonymously();
+      } catch ({ message }) {
+        alert(message);
+      }
+    }
+  };
+
+  get uid() {
+    return (firebase.auth().currentUser || {}).uid;
+  }
+
+  get ref() {
+    return firebase.database().ref("messages");
+  }
+
+  parse = (snapshot) => {
+    const { timestamp: numberStamp, text, user } = snapshot.val();
+    const { key: _id } = snapshot;
+    const timestamp = new Date(numberStamp);
+    const message = {
+      _id,
+      timestamp,
+      text,
+      user,
+    };
+    return message;
+  };
+
+  on = (callback) =>
+    this.ref
+      .limitToLast(20)
+      .on("child_added", (snapshot) => callback(this.parse(snapshot)));
+
+  get timestamp() {
+    return firebase.database.ServerValue.TIMESTAMP;
+  }
+
+  // send the message to the Backend
+  send = (messages) => {
+    for (let i = 0; i < messages.length; i++) {
+      const { text, user } = messages[i];
+      const message = {
+        text,
+        user,
+        timestamp: this.timestamp,
+      };
+      this.append(message);
+    }
+  };
+
+  append = (message) => this.ref.push(message);
+
+  // close the connection to the Backend
+  off() {
+    this.ref.off();
+  }
+}
+
+Fire.shared = new Fire();
+export default Fire;
