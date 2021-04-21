11c85e3d45db9d37398896dd08f3fe72ca4899a6
Joey Yu <itsjoeoui@gmail.com>
2021-04-20 20:24:39 -0400

    Add a function to add multiple trees at once
    * Currently the locations are still hardcoded, but the basic idea is there.
    * Also add lodash and re-install firebase.
    * Reference: https://stackoverflow.com/questions/49078941/markers-not-rendering-on-mapview-with-expo.
    
    Signed-off-by: Joey Yu <itsjoeoui@gmail.com>

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,9 +2,52 @@ import React from "react";
 import { StyleSheet, Dimensions, Image } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack";
 import MapView, { Marker } from "react-native-maps";
-import tree from "../../assets/tree.png";
+import _ from "lodash";
+import treeMarker from "../../assets/tree.png";
 
 function HomeScreen({ navigation }) {
+  // TODO Fetch tree data from Firebase.
+  // TODO Navigate to diff chat window when diff tree is pressed.
+  const trees = [
+    {
+      key: 1,
+      coordinate: {
+        latitude: 45.488624,
+        longitude: -73.587916,
+      },
+      onPress: () => navigation.navigate("Chat"),
+    },
+    {
+      key: 2,
+      coordinate: {
+        latitude: 45.490044,
+        longitude: -73.588936,
+      },
+      onPress: () => navigation.navigate("Chat"),
+    },
+    {
+      key: 3,
+      coordinate: {
+        latitude: 45.490644,
+        longitude: -73.587236,
+      },
+      onPress: () => navigation.navigate("Chat"),
+    },
+  ];
+
+  const renderTrees = () => {
+    const renderedTrees = _.map(trees, (tree) => {
+      const { key, coordinate, onPress } = tree;
+      return (
+        <Marker key={key} coordinate={coordinate} onPress={onPress}>
+          <Image style={styles.tree} source={treeMarker} />
+        </Marker>
+      );
+    });
+    return renderedTrees;
+  };
+
+  // TODO Set the initialRegion based on user's location.
   return (
     <MapView
       style={styles.map}
@@ -15,15 +58,7 @@ function HomeScreen({ navigation }) {
         longitudeDelta: 0.0034,
       }}
     >
-      <Marker
-        coordinate={{
-          latitude: 45.489724,
-          longitude: -73.587916,
-        }}
-        onPress={() => navigation.navigate("Chat")}
-      >
-        <Image style={styles.tree} source={tree} />
-      </Marker>
+      {renderTrees()}
     </MapView>
   );
 }
